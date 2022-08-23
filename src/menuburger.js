ef9cd1fa2c7a467b4e009fac46f99ee15c5f0c39
youcefyn8 <youcef.nemmiche@gmail.com>
2022-08-23 11:01:12 +0200

    ecf html css

---
+++ src/menuburger.js
@@ -0,0 +1,9 @@
+function afficherMenu() {
+    let refMenu = document.getElementById("menu");
+    refMenu.style.left="0";
+}
+
+function cacherMenu() {
+    let refMenu = document.getElementById("menu");
+    refMenu.style.left="-100%";
+}
